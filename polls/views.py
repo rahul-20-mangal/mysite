ed069d1c8fb141d2673bb9bec018637d70915d52
Rahul Mangal <rahul200826101998@gmail.com>
2021-03-31 12:01:58 +0530

    Setting Up the Indain Time Zone

--- polls/views.py
+++ polls/views.py
@@ -4,10 +4,3 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-def something(request):
-    return HttpResponse("hi there this is a test ")
-
-def anotherSomething(request):
-    return HttpResponse(
-        "bla bla blaaa"
-    )
