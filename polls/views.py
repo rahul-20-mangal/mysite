4aa5d085d525ac6ee0d59bc14d9ca4cfbfb41ef1
Rahul Mangal <rahul200826101998@gmail.com>
2021-03-31 10:53:14 +0530

    This is my first commit message from visual studio code

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def something(request):
+    return HttpResponse("hi there this is a test ")
