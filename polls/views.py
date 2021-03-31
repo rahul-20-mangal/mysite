0816037797484b5224982dbf51b6f291f5cd85b5
Rahul Mangal <rahul200826101998@gmail.com>
2021-03-31 16:03:14 +0530

    Add detail function to polls views
    Details about particular question

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
